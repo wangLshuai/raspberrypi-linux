db7adccfcf51e2d88153e65c071afbb56c08f18d
wangshuai <wang.lin.shuai@qq.com>
2024-07-04 00:09:50 +0800

    test resources of platform device

--- myproject/of_test/of_test.c
+++ myproject/of_test/of_test.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/of_address.h>
 
 int process_platform_device(struct device *device, void *data)
 {
@@ -16,11 +17,15 @@ static int __init of_test_init(void)
 	struct device_node *np;
 	struct device_node *child;
 	struct property *pp;
-	int len, i;
+	int len, i, num;
 	char value[128];
 	char property_str[128];
 	struct device *dev;
 	struct platform_device *pdev;
+	const __be32 *addrp;
+	u64 size;
+	unsigned int flags;
+	u64 taddr;
 	printk("of_test_init\n");
 	np = of_find_node_by_path("/");
 	printk("root of node full name:%s\n", np->full_name);
@@ -54,12 +59,56 @@ static int __init of_test_init(void)
 		printk("find leds  platform device\n");
 		pdev = to_platform_device(dev);
 		len = pdev->num_resources;
+		printk("platform device resource num:%d\n", len);
+		if (dev->driver != NULL) {
+			printk("match driver name dev->driver->name:%s\n",
+			       dev->driver->name);
+		}
+	}
+
+	np = of_find_node_by_name(NULL, "gpiomem");
+	pp = of_find_property(np, "reg", &len);
+	if (pp) {
+		printk("reg property len:%d\n", len);
+		print_hex_dump(KERN_INFO, "", DUMP_PREFIX_OFFSET, 1, 16,
+			       pp->value, len, 1);
+	}
+	if (np) {
+		printk("find %s device node\n", np->name);
+		num = of_address_count(np);
+		printk("resource count:%d\n", num);
+		for (i = 0; i < num; i++) {
+			printk("resource %d\n", i);
+			// of_address_to_resource(np, i, &res);
+			// printk("start:%llx end:%llx\n", res.start, res.end);
+			addrp = __of_get_address(np, i, -1, &size, &flags);
+			if (flags & IORESOURCE_MEM) {
+				taddr = of_translate_address(np, addrp);
+				printk("taddr:%llx\n", taddr);
+			} else if (flags & IORESOURCE_IO)
+				printk("IORESOURCE_IO\n");
+			else
+				return -EINVAL;
+		}
+	}
+
+	dev = bus_find_device_by_name(&platform_bus_type, NULL, "fe200000.gpiomem");
+	if (dev != NULL) {
+		pdev = to_platform_device(dev);
+		printk("find %s  platform device\n", pdev->name);
+		len = pdev->num_resources;
 		printk("led platform device resource num:%d\n", len);
 		if (dev->driver != NULL) {
 			printk("match name dev->driver->name:%s\n",
 			       dev->driver->name);
 		}
+		for (i = 0; i < len; i++) {
+			printk("resource %d res->name:%s res->start:%llx ,res->end:%llx\n",
+			       i, pdev->resource[i].name,pdev->resource[i].start,
+			       pdev->resource[i].end);
+		}
 	}
+
 	return 0;
 }
 
