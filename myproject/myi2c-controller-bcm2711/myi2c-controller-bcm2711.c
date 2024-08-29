b860bab03c55cdb6e497c316b4002d572411e1ca
wangshuai <wang.lin.shuai@qq.com>
2024-08-29 17:49:27 +0800

    register a i2c adapter

--- myproject/myi2c-controller-bcm2711/myi2c-controller-bcm2711.c
+++ myproject/myi2c-controller-bcm2711/myi2c-controller-bcm2711.c
@@ -1,3 +1,7 @@
+#include "linux/device.h"
+#include "linux/gfp_types.h"
+#include "linux/i2c.h"
+#include "linux/irqnr.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -7,14 +11,41 @@
 #include <linux/pinctrl/consumer.h>
 #include <linux/pinctrl/devinfo.h>
 #include <linux/of.h>
+#include <linux/of_irq.h>
 
 static struct of_device_id i2c_controller_id_tables[] = {
 	{
-	 .compatible = "brcm,bcm2711-i2c",
-	  },
-	{ }
+		.compatible = "brcm,bcm2711-i2c",
+	},
+	{}
 };
 
+struct i2c_control {
+	struct i2c_adapter adap;
+};
+
+static int i2c_controller_master_xfer(struct i2c_adapter *adap,
+				      struct i2c_msg *msgs, int num)
+{
+	return 0;
+}
+static int i2c_controller_master_xfer_atomic(struct i2c_adapter *adap,
+					     struct i2c_msg *msgs, int num)
+{
+	return 0;
+}
+
+u32 i2c_controller_algo_support(struct i2c_adapter *adap)
+{
+	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL | I2C_FUNC_PROTOCOL_MANGLING;
+}
+static struct i2c_algorithm algo = {
+	.functionality = i2c_controller_algo_support,
+	.master_xfer = i2c_controller_master_xfer,
+	.master_xfer_atomic = i2c_controller_master_xfer_atomic,
+	// .master_xfer_msg = i2c_controller_master_xfer_msg,
+	// .master_xfer_msg_atomic = i2c_controller_master_xfer_msg_atomic,
+};
 static int i2c_controller_probe(struct platform_device *pdev)
 {
 	struct device_node *node = dev_of_node(&pdev->dev);
@@ -28,6 +59,7 @@ static int i2c_controller_probe(struct platform_device *pdev)
 	struct pinctrl_state *state;
 	int i, count;
 	const char *str;
+	struct irq_desc *irq_desc;
 
 	for_each_property_of_node(node, pp) {
 		printk("property name:%s\n", pp->name);
@@ -70,13 +102,52 @@ static int i2c_controller_probe(struct platform_device *pdev)
 
 		pinctrl_put(pc);
 	}
+
+	struct of_phandle_args args;
+	u32 intsize;
+	of_irq_parse_one(node, 0, &args);
+	mylog("args:%s %d %d %d\n", args.np->name, args.args[0], args.args[1],
+	      args.args[2]);
+	struct device_node *interrupt = of_irq_find_parent(node);
+	of_node_put(interrupt);
+	mylog("interrupt:%s\n", interrupt->name);
+
+	if (!of_property_read_u32(args.np, "#interrupt-cells", &intsize))
+		mylog("intsize:%d", intsize);
+	int irq = platform_get_irq(pdev, 0);
+	irq_desc = irq_to_desc(irq);
+	mylog("irq:%d hw_irq:%lu interrupt controller:%s\n", irq,
+	      irq_desc->irq_data.hwirq, irq_desc->irq_data.domain->name);
+	mylog("of_irq_get:%d\n", of_irq_get(node, 0));
+
+	struct i2c_control *i2c_control = devm_kzalloc(
+		&pdev->dev, sizeof(struct i2c_control), GFP_KERNEL);
+	platform_set_drvdata(pdev, &i2c_control->adap);
+	snprintf(i2c_control->adap.name, sizeof(i2c_control->adap.name),
+		 "bcm2835 (%s)", of_node_full_name(pdev->dev.of_node));
+	i2c_control->adap.owner = THIS_MODULE;
+	i2c_control->adap.class = I2C_CLASS_DEPRECATED;
+	i2c_control->adap.algo = &algo;
+	i2c_control->adap.dev.parent = &pdev->dev;
+	i2c_control->adap.dev.of_node = pdev->dev.of_node;
+
+	// add a i2c bus,and foreach child node to register i2c_client.
+	i2c_add_adapter(&i2c_control->adap);
 	return 0;
 }
 
+int i2c_controller_remove(struct platform_device *pdev)
+{
+	struct i2c_adapter *adap = platform_get_drvdata(pdev);
+
+	i2c_del_adapter(adap);
+	return 0;
+}
 static struct platform_driver i2c_control = {
-	.driver = {.of_match_table = i2c_controller_id_tables,
-		   .name = "myi2c_controller_driver" },
+	.driver = { .of_match_table = i2c_controller_id_tables,
+		    .name = "myi2c_controller_driver" },
 	.probe = i2c_controller_probe,
+	.remove = i2c_controller_remove,
 };
 
 MODULE_DEVICE_TABLE(of, i2c_controller_id_tables);
