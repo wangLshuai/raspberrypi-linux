8200a348a631c6d7ac1415c7878ac12c5ea302bd
wangshuai <wang.lin.shuai@qq.com>
2024-09-11 21:34:09 +0800

    add i2c adapter master_xfer function
    It's not working now, there are bugs.
    TW interrupt is level triger,xfer will be block by continus interrupt after start.
    read or write fifo must run in irq.

--- myproject/myi2c-controller-bcm2711/myi2c-controller-bcm2711.c
+++ myproject/myi2c-controller-bcm2711/myi2c-controller-bcm2711.c
@@ -1,3 +1,5 @@
+#include <linux/completion.h>
+#include <linux/jiffies.h>
 #include <linux/container_of.h>
 #include <linux/ioport.h>
 #include <linux/i2c.h>
@@ -17,14 +19,36 @@
 
 #define I2C_CONTROL_REG 0x0
 #define I2C_STATUS_REG 0x4
-#define I2C_DLEN_REG 0x8
 #define I2C_ADDR_REG 0xc // slave address
+#define I2C_DLEN_REG 0x8
 #define I2C_FIFO_REG 0x10 // data fifo
 
 // i2c clock divider CDIV is always rounded down to aneven number
 #define I2C_DIV_REG 0x14
 #define I2C_DELAY_REG 0x18 // data delay
 #define I2C_CLKT_REG 0x1c // clock stretch timeout
+
+// status bit
+#define S_CLKT_BIT BIT(9)
+#define S_ERR_BIT BIT(8)
+#define S_RXF_BIT BIT(7)
+#define S_TRE_BIT BIT(6)
+#define S_RXD_BIT BIT(5)
+#define S_TXD_BIT BIT(4)
+#define S_RXR_BIT BIT(3)
+#define S_TXW_BIT BIT(2)
+#define S_DONE_BIT BIT(1)
+#define S_TA_BIT BIT(0)
+
+// control bit
+#define C_EN_BIT BIT(15)
+#define C_INTR_BIT BIT(10)
+#define C_INTT_BIT BIT(9)
+#define C_INTD_BIT BIT(8)
+#define C_START_BIT BIT(7)
+#define C_CLEAR_BIT BIT(5)
+#define C_READ_BIT BIT(0)
+
 static struct of_device_id i2c_controller_id_tables[] = {
 	{
 		.compatible = "brcm,bcm2711-i2c",
@@ -35,18 +59,100 @@ static struct of_device_id i2c_controller_id_tables[] = {
 struct i2c_controller {
 	struct i2c_adapter adap;
 	struct clk_hw i2c_bus_hw_clk;
+	int irq;
+	struct completion rw_completion;
+	struct completion done;
 	void *reg;
 };
 
+static int transfer_msg(struct i2c_msg *msg, struct i2c_controller *i2c_ctrl)
+{
+	iowrite32(msg->addr, i2c_ctrl->reg + I2C_ADDR_REG);
+	iowrite32(msg->len, i2c_ctrl->reg + I2C_DLEN_REG);
+	printk("msg len:%d\n", msg->len);
+	if (msg->flags & I2C_M_RD) {
+		printk("start read %d bytes\n", msg->len);
+		iowrite32(C_EN_BIT | C_START_BIT | C_READ_BIT | C_INTD_BIT |
+				  C_INTR_BIT,
+			  i2c_ctrl->reg + I2C_CONTROL_REG);
+		int i = 0;
+		while (i < msg->len) {
+			u32 status = ioread32(i2c_ctrl->reg + I2C_STATUS_REG);
+			if (status & S_RXD_BIT) {
+				msg->buf[i] =
+					ioread32(i2c_ctrl->reg + I2C_FIFO_REG);
+				printk("read a byte:%x\n", msg->buf[i]);
+				i++;
+			} else {
+				if (msg->len - i >= 12) {
+					if (!wait_for_completion_timeout(
+						    &i2c_ctrl->rw_completion,
+						    msecs_to_jiffies(100))) {
+						printk("timeout\n");
+						break;
+					}
+				} else {
+					if (!wait_for_completion_timeout(
+						    &i2c_ctrl->done,
+						    msecs_to_jiffies(100))) {
+						printk("timeout\n");
+						break;
+					}
+				}
+			}
+		}
+
+	} else {
+		iowrite32((~C_READ_BIT) &
+				  (C_EN_BIT | C_CLEAR_BIT | C_START_BIT |
+				   C_INTD_BIT | C_INTT_BIT),
+			  i2c_ctrl->reg + I2C_CONTROL_REG);
+
+		int i = 0;
+		while (i < msg->len) {
+			u32 status = ioread32(i2c_ctrl->reg + I2C_STATUS_REG);
+			if (status & S_TXD_BIT) {
+				printk("write a byte:%x\n", msg->buf[i]);
+				iowrite32(msg->buf[i],
+					  i2c_ctrl->reg + I2C_FIFO_REG);
+				printk("writed a byte:%x\n", msg->buf[i]);
+				i++;
+			} else {
+				if (!wait_for_completion_timeout(
+					    &i2c_ctrl->rw_completion,
+					    msecs_to_jiffies(100))) {
+					printk("timeout\n");
+					break;
+				}
+			}
+		}
+
+		printk("wait tx complete\n");
+		u32 status = ioread32(i2c_ctrl->reg + I2C_STATUS_REG);
+		if (status & S_TA_BIT) {
+			if (!wait_for_completion_timeout(
+				    &i2c_ctrl->done, msecs_to_jiffies(100))) {
+				pr_err("wait tx complete timeout\n");
+			}
+
+		} else {
+			init_completion(&i2c_ctrl->rw_completion);
+		}
+	}
+
+	iowrite32(C_CLEAR_BIT, i2c_ctrl->reg + I2C_CONTROL_REG);
+	return 0;
+}
 static int i2c_controller_master_xfer(struct i2c_adapter *adap,
 				      struct i2c_msg *msgs, int num)
 {
-	return 0;
-}
-static int i2c_controller_master_xfer_atomic(struct i2c_adapter *adap,
-					     struct i2c_msg *msgs, int num)
-{
-	return 0;
+	struct i2c_controller *i2c_controller =
+		container_of(adap, struct i2c_controller, adap);
+	iowrite32(C_CLEAR_BIT, i2c_controller->reg + I2C_CONTROL_REG);
+	for (int i = 0; i < num; i++) {
+		transfer_msg(&msgs[i], i2c_controller);
+	}
+	return num;
 }
 
 static u32 i2c_controller_algo_support(struct i2c_adapter *adap)
@@ -79,9 +185,10 @@ static unsigned long recalc_get_rate(struct clk_hw *hw,
 long round_rate(struct clk_hw *hw, unsigned long rate,
 		unsigned long *parent_rate)
 {
-	mylog("parent_rate:%llu,rate:%lu\n", *parent_rate, rate);
-
-	return rate;
+	u32 div = DIV_ROUND_UP(*parent_rate, rate);
+	if (div & 1)
+		div--;
+	return DIV_ROUND_UP(*parent_rate, div);
 }
 
 int set_rate(struct clk_hw *hw, unsigned long rate, unsigned long parent_rate)
@@ -89,8 +196,26 @@ int set_rate(struct clk_hw *hw, unsigned long rate, unsigned long parent_rate)
 	struct i2c_controller *i2c_controller =
 		container_of(hw, struct i2c_controller, i2c_bus_hw_clk);
 	u32 div = parent_rate / rate;
+	div = max(2, div);
+	div = min(0xffff, div);
 	mylog("parent_rate:%lu,rate:%lu,div:%x\n", parent_rate, rate, div);
 	iowrite32(div, i2c_controller->reg + I2C_DIV_REG);
+
+	// a i2c bus clock cycle  = div parent clock cycles
+	// high level or lowel level,half cycle = div/2 div parent clock cycles
+	// change sda when sck low level
+	// controller changer sda level after falling edge when send next bit
+	// controller sample sda level after rise edge when controller receive next bit
+
+	//
+	u16 fedl = max(div / 4, 1);
+	u16 redl = max(div / 4, 1);
+	iowrite32((fedl << 16) | redl, i2c_controller->reg + I2C_DELAY_REG);
+
+	// Number of SCL clock cycles to wait after the rising edge of
+	// SCL before deciding that the slave is not responding
+	iowrite32(div, i2c_controller->reg + I2C_CLKT_REG);
+
 	return 0;
 }
 static struct clk_ops clk_ops = {
@@ -99,79 +224,41 @@ static struct clk_ops clk_ops = {
 	.set_rate = set_rate,
 };
 
+static irqreturn_t i2c_irq_handler(int irq, void *data)
+{
+	struct i2c_controller *i2c_controller = data;
+	u32 status = ioread32(i2c_controller->reg + I2C_STATUS_REG);
+	u32 value = ioread32(i2c_controller->reg + I2C_CONTROL_REG);
+	printk("status:0x%x value:0x%x", status & 0xffff, value & 0xffff);
+	if (status & S_DONE_BIT) {
+		printk("done\n");
+		iowrite32(S_DONE_BIT, i2c_controller->reg + I2C_STATUS_REG);
+		complete(&i2c_controller->done);
+	}
+	if ((status & S_RXR_BIT) || (status & S_TXW_BIT)) {
+		printk("irq rw\n");
+		status = ioread32(i2c_controller->reg + I2C_DLEN_REG);
+		printk("len:%u\n", status);
+		complete(&i2c_controller->rw_completion);
+
+		// ?????????? bug
+		// iowrite32(C_CLEAR_BIT, i2c_controller->reg + I2C_CONTROL_REG);
+	}
+
+	if (status & S_CLKT_BIT) {
+		iowrite32(S_CLKT_BIT, i2c_controller->reg + I2C_STATUS_REG);
+		pr_err("CLKT\n");
+	}
+	if (status & S_ERR_BIT) {
+		iowrite32(S_ERR_BIT, i2c_controller->reg + I2C_STATUS_REG);
+		pr_err("error\n");
+	}
+
+	return IRQ_HANDLED;
+}
 static int i2c_controller_probe(struct platform_device *pdev)
 {
-	struct device_node *node = dev_of_node(&pdev->dev);
-	struct property *pp;
-	const __be32 *addrp;
-	u64 phy_addr;
-	u64 size;
-	uint flags;
-	struct resource res;
-	struct pinctrl *pc;
-	struct pinctrl_state *state;
-	int i, count, ret;
-	const char *str;
-	struct irq_desc *irq_desc;
-
-	for_each_property_of_node(node, pp) {
-		printk("property name:%s\n", pp->name);
-	}
-
-	printk("address count:%d\n", of_address_count(node));
-	addrp = of_get_address(node, 0, &size, &flags);
-	print_hex_dump(KERN_INFO, "reg value", DUMP_PREFIX_OFFSET, 16, 1, addrp,
-		       of_n_addr_cells(node) * 4, false);
-	of_address_to_resource(node, 0, &res);
-	mylog("resource:%llx size:%llx\n", res.start, res.end - res.start + 1);
-	phy_addr = of_translate_address(node, addrp);
-	mylog("phy_addr:%llx\n", phy_addr);
-
-	// really_probe call pinctrl_bind_pins when device match driver,set dev->pins
-	// and select init or
-	pc = pinctrl_get(&pdev->dev);
-	if (!IS_ERR(pc)) {
-		state = pinctrl_lookup_state(pc, "default");
-		if (!IS_ERR(state)) {
-			mylog("state == dev->pins.defaults:%d\n",
-			      state == pdev->dev.pins->default_state);
-		}
-
-		count = of_property_count_strings(node, "pinctrl-names");
-		for (i = 0; i < count; i++) {
-			of_property_read_string_index(node, "pinctrl-names", i,
-						      &str);
-			state = pinctrl_lookup_state(pc, str);
-			if (!IS_ERR(state)) {
-				mylog("state name:%s\n", str);
-			}
-		}
-
-		state = pinctrl_lookup_state(pc, "sleep");
-		if (!IS_ERR(state)) {
-			mylog("i2c select sleep pinctrl test\n");
-			pinctrl_select_state(pc, state);
-		}
-
-		pinctrl_put(pc);
-	}
-
-	struct of_phandle_args args;
-	u32 intsize;
-	of_irq_parse_one(node, 0, &args);
-	mylog("args:%s %d %d %d\n", args.np->name, args.args[0], args.args[1],
-	      args.args[2]);
-	struct device_node *interrupt = of_irq_find_parent(node);
-	of_node_put(interrupt);
-	mylog("interrupt:%s\n", interrupt->name);
-
-	if (!of_property_read_u32(args.np, "#interrupt-cells", &intsize))
-		mylog("intsize:%d", intsize);
-	int irq = platform_get_irq(pdev, 0);
-	irq_desc = irq_to_desc(irq);
-	mylog("irq:%d hw_irq:%lu interrupt controller:%s\n", irq,
-	      irq_desc->irq_data.hwirq, irq_desc->irq_data.domain->name);
-	mylog("of_irq_get:%d\n", of_irq_get(node, 0));
+	int ret;
 
 	mylog("+++++++++++++++++++++++++++++start probe i2c controller\n");
 	int bus_clk_rate;
@@ -185,10 +272,12 @@ static int i2c_controller_probe(struct platform_device *pdev)
 	i2c_controller->adap.algo = &algo;
 	i2c_controller->adap.dev.parent = &pdev->dev;
 	i2c_controller->adap.dev.of_node = pdev->dev.of_node;
+	init_completion(&i2c_controller->rw_completion);
+	init_completion(&i2c_controller->done);
 
 	struct resource *reg = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	i2c_controller->reg = devm_ioremap_resource(&pdev->dev, reg);
-	mylog("reg->start:%llx\n", reg->start);
+	mylog("reg:%llx\n", (uint64_t)i2c_controller->reg);
 	// get parent clock , clocks = <&clocks BCM2835_CLOCK_VPU> in /soc/i2c1: i2c@7e804000
 	// clocks == of_clk_provider->node
 	// clk_hw_create_clk(of_clk_provider->data->hws[BCM2835_CLOCK_VPU])
@@ -229,23 +318,38 @@ static int i2c_controller_probe(struct platform_device *pdev)
 	mylog("current rate:%lu\n",
 	      clk_get_rate(i2c_controller->i2c_bus_hw_clk.clk));
 	// call parent ops->prepare and ops->enable if function no null
-	ret = clk_prepare_enable(i2c_controller->i2c_bus_hw_clk.clk);
+
+	i2c_controller->irq = platform_get_irq(pdev, 0);
+	ret = request_irq(i2c_controller->irq, i2c_irq_handler, IRQF_SHARED,
+			  "i2c-controller", i2c_controller);
 	if (ret) {
-		dev_err(&pdev->dev, "Couldn't prepare clock");
+		pr_err("request irq failed,ret:%d\n", ret);
 		return ret;
 	}
 
+	ret = clk_prepare_enable(i2c_controller->i2c_bus_hw_clk.clk);
+	if (ret) {
+		free_irq(i2c_controller->irq, i2c_controller);
+		dev_err(&pdev->dev, "Couldn't prepare clock");
+		return ret;
+	}
 	// add a i2c bus,and foreach child node to register i2c_client.
 	ret = i2c_add_adapter(&i2c_controller->adap);
-	// if (ret)
-	// 	clk_unprepare(i2c_controller->i2c_bus_hw_clk.clk);
-	return 0;
+	if (ret)
+		clk_unprepare(i2c_controller->i2c_bus_hw_clk.clk);
+
+	return ret;
 }
 
 int i2c_controller_remove(struct platform_device *pdev)
 {
 	struct i2c_adapter *adap = platform_get_drvdata(pdev);
 
+	struct i2c_controller *i2c_controller =
+		container_of(adap, struct i2c_controller, adap);
+	free_irq(i2c_controller->irq, i2c_controller);
+	clk_rate_exclusive_put(i2c_controller->i2c_bus_hw_clk.clk);
+	clk_disable_unprepare(i2c_controller->i2c_bus_hw_clk.clk);
 	i2c_del_adapter(adap);
 	return 0;
 }
